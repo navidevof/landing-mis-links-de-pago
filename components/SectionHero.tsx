77c24ed8734bfd3bea44f07cb0061723a56579b5
Navidev <navi.devoficial@gmail.com>
2024-11-15 18:40:43 -0500

    Refactor: Change text hero

--- components/SectionHero.tsx
+++ components/SectionHero.tsx
@@ -4,7 +4,7 @@ function SectionHero() {
   return (
     <section className="flex flex-col gap-y-3 md:gap-y-4 items-center justify-center py-10 md:py-20 md:bg-hero-gradient-desktop bg-hero-gradient-mb bg-center">
       <h1 className="text-4xl md:text-6xl font-bold text-center w-10/12 mx-auto max-w-screen-lg text-balance">
-        Centraliza tus métodos de pago en un solo enlace
+        Centraliza tus métodos de pago en un solo lugar
       </h1>
       <p className="text-center max-w-xl text-lg mx-auto text-pretty w-10/12 mb-4 md:text-2xl">
         Transforma tus métodos de pago en una experiencia sencilla, rápida y
